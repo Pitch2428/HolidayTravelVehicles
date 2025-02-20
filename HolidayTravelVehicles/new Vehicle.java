fd5fd1cea81e8238c9a663e97e467980698fc5fc
Pitch2428 <124357506+Pitch2428@users.noreply.github.com>
2025-02-20 13:48:44 +0700

    Delete HolidayTravelVehicles/new Vehicle.java

--- HolidayTravelVehicles/new Vehicle.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Vehicle myCar = new Vehicle("12345", "Sedan", "Model S", 2022, "Tesla", 79999.99);
-        myCar.displayInfo();
-    }
-}
